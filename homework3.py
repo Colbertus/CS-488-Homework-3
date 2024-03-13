8e300c36797bacce1e970919da486bf107c266a7
Colby McClure <colbymc0801@gmail.com>
2024-03-12 20:28:27 -0500

    Further implementation

--- homework3.py
+++ homework3.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import decomposition
 from sklearn import datasets 
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
 
 # Load the data
 df = loadmat('indianR.mat')
@@ -24,3 +26,37 @@ bands, samples = x.shape
 gth_mat = loadmat('indian_gth.mat')
 gth_mat = {i : j for i, j in gth_mat.items() if i[0] != '_'}
 gt = pd.DataFrame({i : pd.Series(j[0]) for i, j in gth_mat.items()})
+
+# List features 
+n = []
+ind = [] 
+for i in range(bands):
+    n.append(i + 1)
+
+for i in range(bands):
+    ind.append('band' + str(n[i]))
+
+features = ind
+
+# Normalize the features (preprocessing)
+# 'MinMaxScaler' is a form of normalization that scales the data to a range between 0 and 1
+scaler_model = MinMaxScaler()
+scaler_model.fit(x.astype(float))
+x = scaler_model.transform(x) 
+
+# Apply PCA to the normalized features
+pca = PCA(n_components = 10)
+principalComponents = pca.fit_transform(x)
+
+# Display contribution of each principal component
+ev = pca.explained_variance_ratio_
+#print(ev) 
+
+# Convert the principal components to a DataFrame
+principalDf = pd.DataFrame(data = principalComponents, columns = ['PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7', 'PC8', 'PC9', 'PC10'])
+
+# Concatenate the principal components with the ground truth data
+finalDf = pd.concat([principalDf, gt], axis = 1)
+
+# Print the DataFrame
+print(principalDf)
